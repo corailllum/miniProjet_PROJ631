b5f12718481d6637a5db3c39b1c32e221980274c
corailllum <160240585+corailllum@users.noreply.github.com>
2024-04-01 20:16:15 +0200

    ajout de la creation des fichier alphabets et fichier compressée

--- Miniprojet1_PROJ631/src/test.java
+++ Miniprojet1_PROJ631/src/test.java
@@ -5,11 +5,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.io.IOException;
+import java.io.BufferedWriter;
 
 public class test {
 	public static void main(String[] args) {
-		//File fichier=new File("textesimple", "C:\\Users\\chanu\\Documents\\polytech\\3emeAnnee\\PROJ631\\files");
-		Path f =Paths.get("C:\\Users\\chanu\\Documents\\polytech\\3emeAnnee\\PROJ631\\files\\extraitalice.txt");
+		//partie du code permettant la lecture du fichier qui sera par la suite codée
+		
+		//entre les parenthese du get renseignée le chemin complet du fichier a codée 
+		//  /!\/!\TOUS LES BACKSLASH DU CHEMIN DOIVENT ETRE DOUBLEE /!\/!\ (exemple avec mon propre chemin de fichier)
+		Path f =Paths.get("C:\\Users\\chanu\\Documents\\polytech\\3emeAnnee\\PROJ631\\files\\textesimple.txt");
 		System.out.println(f);
 		String test2="";
 		try {
@@ -19,13 +23,9 @@ public class test {
 			
 			while((test=bfr.readLine())!=null) {
 				test2+=test;
-				System.out.println("lecture de variable \n"+test);
 			}
-			System.out.println(test2);
 			bfr.close();
-			
 		}
-		
 		catch(IOException e){
 			System.err.println("IOexception");
 		}
@@ -33,37 +33,78 @@ public class test {
 			System.err.println("erreur impossible de lire les ligne du fichier ");
 		}
 		
+		//partie qui permet de verifier de faire l'encodage du fichier rentré en paramettre 
+		
 		Texte mot= new Texte(test2);
-		System.out.println(mot.getTexts());
+		/*System.out.println(mot.getTexts());
 		System.out.println(mot.getNbCaractere());
 		System.out.println(mot.getTableauTexteNormal());
 		System.out.println(mot.getTabDeCaractere());
 		System.out.println(mot.getTabNbIteration());
 		System.out.println(mot.getTabChararctereHuffman());
-		System.out.println(mot.getTabIterationHuffman());
+		System.out.println(mot.getTabIterationHuffman());*/
 		
 		
-		
+		//creation de tous les arbre binaire ainsi que leur report dans une liste qui sera donc la liste de toute les feuilles de l'arbre 
 		ArrayList<ArbreBinaire> listeNoeud=new ArrayList<ArbreBinaire>();
 		for (int i=0; i<mot.getTabIterationHuffman().size();i++) {
 			listeNoeud.add(new ArbreBinaire(mot.getTabIterationHuffman().get(i)));
 			listeNoeud.get(i).setLettreAssocier(mot.getTabChararctereHuffman().get(i));
-			System.out.println(listeNoeud.get(i).getLettreAssocier());
-			System.out.println(listeNoeud.get(i).getEtiquette());
+			//System.out.println(listeNoeud.get(i).getLettreAssocier());
+			//System.out.println(listeNoeud.get(i).getEtiquette());
 			
 		}
-		System.out.println("taille des feuille "+listeNoeud.size());
+		//System.out.println("taille des feuille "+listeNoeud.size());
 		ArbreHuffman arb=new ArbreHuffman(listeNoeud);
 		
 		arb.creationArbreComplet();
 		
 		arb.chiffrement(arb.getListetempoNoeud().get(0), arb.getListechiffrement(), arb.getListeLettrechiffree(), "");
-		System.out.println(arb.getListeLettrechiffree());
-		System.out.println(arb.getListechiffrement());
+		//System.out.println(arb.getListeLettrechiffree());
+		//System.out.println(arb.getListechiffrement());
 		arb.codageDuTexte(mot);
-		System.out.println(arb.getTextechiffree());
-		System.out.println(arb.calculeGainFinal(mot));
-		System.out.println(arb.calculeTauxCompressionMoyen());
+		//System.out.println(arb.getTextechiffree());
+		//System.out.println(arb.calculeGainFinal(mot));
+		//System.out.println(arb.calculeTauxCompressionMoyen());
+		
+		
+		
+		//Partie du code qui permet de crée les fichier alphabet ainsi que le document codée en lui meme 
+		
+		try {
+			//entre les parenthese du get renseignée le chemin complet de l'emplacemnt du futur fichier codée ainsi que le fichier de l'alphabets
+			//  /!\/!\TOUS LES BACKSLASH DU CHEMIN DOIVENT ETRE DOUBLEE /!\/!\ (exemple avec mon propre chemin de fichier)
+			Path falphabet =Paths.get("C:\\Users\\chanu\\Documents\\polytech\\3emeAnnee\\PROJ631\\files\\alphabets.txt");
+			Path fcompr =Paths.get("C:\\Users\\chanu\\Documents\\polytech\\3emeAnnee\\PROJ631\\files\\textecompresser.txt");
+			BufferedWriter bfwalpha=Files.newBufferedWriter(falphabet);
+			BufferedWriter bfwcompr=Files.newBufferedWriter(fcompr);
+			
+			//ecriture du fichier comprenant l'entierter du fichier codée
+			bfwcompr.write(arb.getTextechiffree());
+			//ecriture du fichier comptent l'alphabets le gain anisi que le taux moyen de compression d'une lettre
+			bfwalpha.write("il y a "+mot.getNbCaractere()+" caracterts qui on été codées");
+			bfwalpha.newLine();
+			bfwalpha.write("la taille moyenne de chaque caractere codée est de :"+arb.calculeTauxCompressionMoyen()+" bits");
+			bfwalpha.newLine();
+			bfwalpha.write("le taux de compression du fichier est de :"+arb.calculeGainFinal(mot)+" %");
+			bfwalpha.newLine();
+			bfwalpha.newLine();
+			bfwalpha.write("l'alphabets utiliser est le suivant :");
+			bfwalpha.newLine();
+			for(int h=0 ; h<arb.getListechiffrement().size();h++) {
+				bfwalpha.write(mot.getTabChararctereHuffman().get(h)+ ": "+mot.getTabIterationHuffman().get(h));
+				bfwalpha.newLine();
+			}
+			
+			bfwalpha.close();
+			bfwcompr.close();
+		}
+		catch(IOException e){
+			System.err.println("IOexception ouverture impossible");
+		}
+		catch(Exception e) {
+			System.err.println("erreur impossible d'ecrire  les ligne du fichier ");
+		}
 	}
 }
 
